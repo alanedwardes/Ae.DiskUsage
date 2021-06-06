5337c2a99bfd44aa1ef633cae553a3d37fa4109c
Alan Edwardes <alan@alanedwardes.com>
2021-06-06 17:55:25 +0100

    Adds code for disk usage analyser.

---
+++ src/Ae.DiskUsage/DiskUsageAnalyser.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ae.DiskUsage
+{
+    public struct LightFileInfo
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public override string ToString() => Name;
+    }
+
+    public sealed class TreeItem
+    {
+        public TreeItem(DirectoryInfo directory, TreeItem parent)
+        {
+            Directory = directory;
+            Parent = parent;
+            Refresh();
+        }
+
+        public ConcurrentBag<LightFileInfo> Files { get; } = new ConcurrentBag<LightFileInfo>();
+        public ConcurrentBag<TreeItem> Children { get; } = new ConcurrentBag<TreeItem>();
+        public DirectoryInfo Directory { get; }
+        public TreeItem Parent { get; }
+
+        private long _cachedSize;
+        private long CalculatedSize => Files.Sum(x => x.Size) + Children.Sum(x => x.CachedSize);
+        public long CachedSize => _cachedSize;
+
+        private IReadOnlyList<TreeItem> _cachedSizes = new TreeItem[0];
+        public IReadOnlyList<TreeItem> CachedChildrenBySizeDescending => _cachedSizes;
+        private IReadOnlyList<TreeItem> CalculatedChildrenBySizeDescending => Children.OrderByDescending(x => x.CachedSize).ToArray();
+
+        private void FilesChanged()
+        {
+            Interlocked.Exchange(ref _cachedSize, CalculatedSize);
+            Interlocked.Exchange(ref _cachedSizes, CalculatedChildrenBySizeDescending);
+        }
+
+        private void ChildrenChanged()
+        {
+            Interlocked.Exchange(ref _cachedSize, Files.Sum(x => x.Size) + Children.Sum(x => x.CachedSize));
+            Interlocked.Exchange(ref _cachedSizes, CalculatedChildrenBySizeDescending);
+        }
+
+        public void Refresh()
+        {
+            if (RefreshTask.Status != TaskStatus.RanToCompletion)
+            {
+                return;
+            }
+
+            RefreshTask = Task.Run(() => Calculate());
+        }
+
+        public bool IsCalculating => RefreshTask.Status != TaskStatus.RanToCompletion;
+
+        private Task RefreshTask { get; set; } = Task.CompletedTask;
+
+        public async Task Calculate()
+        {
+            FileSystemInfo[] children;
+            try
+            {
+                children = Directory.EnumerateFileSystemInfos()
+                    .Where(x => x.IsValidForAnalysis())
+                    .ToArray();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return;
+            }
+
+            Files.Clear();
+
+            foreach (var file in children.OfType<FileInfo>())
+            {
+                Files.Add(new LightFileInfo
+                {
+                    Name = file.Name,
+                    Size = file.Length
+                });
+            }
+
+            Children.Clear();
+
+            foreach (var directory in children.OfType<DirectoryInfo>())
+            {
+                var child = new TreeItem(directory, this);
+                Children.Add(child);
+            }
+
+            foreach (var child in Children)
+            {
+                await child.RefreshTask;
+                child.FilesChanged();
+            }
+
+            var owner = this;
+            while (owner != null)
+            {
+                owner.ChildrenChanged();
+                owner = owner.Parent;
+            }
+        }
+
+        public override string ToString() => Directory.Name;
+    }
+
+    public static class FileSystemInfoExtensions
+    {
+        public static bool IsValidForAnalysis(this FileSystemInfo fileSystemInfo)
+        {
+            return !fileSystemInfo.Attributes.HasFlag(FileAttributes.ReparsePoint);
+        }
+    }
+}
---
+++ src/Ae.DiskUsage/Program.cs
@@ -0,0 +1,97 @@
+﻿using Ae.ImGuiBootstrapper;
+using Humanizer;
+using ImGuiNET;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+using Veldrid;
+using Veldrid.StartupUtilities;
+
+namespace Ae.DiskUsage
+{
+    class Program
+    {
+        static void Main()
+        {
+            var directory = new DirectoryInfo(@"C:\");
+
+            var windowInfo = new WindowCreateInfo(50, 50, 1280, 720, WindowState.Normal, "Ae.DiskUsage");
+
+            TreeItem analyser = null;
+
+            using var window = new ImGuiWindow(windowInfo);
+
+            while (window.Loop(new Vector3(0.45f, 0.55f, 0.6f)))
+            {
+                ImGui.SetNextWindowPos(new Vector2(32, 32));
+                ImGui.SetNextWindowSize(new Vector2(1200, 650));
+                ImGui.Begin($"Results for {directory}");
+
+                if (!analyser?.IsCalculating ?? false)
+                {
+                    if (ImGui.Button("Refresh all"))
+                    {
+                        analyser.Refresh();
+                    }
+                }
+
+                if (analyser == null)
+                {
+                    analyser = new TreeItem(directory, null);
+                }
+                else
+                {
+                    RenderTreeItem(analyser);
+                }
+
+                ImGui.End();
+            }
+        }
+
+        public static void RenderTreeItem(TreeItem treeItem)
+        {
+            foreach (var node in treeItem.CachedChildrenBySizeDescending)
+            {
+                var size = node.CachedSize.Bytes().Humanize("#.#");
+
+                var tag = node.IsCalculating ? $"calculating - {size}" : size;
+
+                var flags = ImGuiTreeNodeFlags.None;
+
+                if (node.Children.Count == 0)
+                {
+                    flags &= ImGuiTreeNodeFlags.Leaf;
+                }
+
+                bool open = ImGui.TreeNodeEx(node.Directory.FullName, flags, $"{node.Directory.Name} ({tag})");
+
+                if (ImGui.BeginPopupContextItem())
+                {
+                    ImGui.Text("Operations");
+                    ImGui.Separator();
+
+                    if (!node.IsCalculating)
+                    {
+                        if (ImGui.Button("Refresh"))
+                        {
+                            node.Refresh();
+                        }
+                    }
+
+                    if (ImGui.Button("Explore"))
+                    {
+                        Process.Start(new ProcessStartInfo { FileName = node.Directory.FullName, UseShellExecute = true, Verb = "open" });
+                    }
+
+                    ImGui.EndPopup();
+                }
+
+                if (open)
+                {
+                    RenderTreeItem(node);
+                    ImGui.TreePop();
+                }
+            }
+        }
+    }
+}
