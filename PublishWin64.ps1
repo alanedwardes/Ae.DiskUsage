b96fc3fd26bf60596777f0a1a485271ab9b6cde8
Alan Edwardes <alan@alanedwardes.com>
2021-06-08 23:31:21 +0100

    Adds PublishWin64.ps1, switches to Humanizer.Core

---
+++ PublishWin64.ps1
@@ -0,0 +1,11 @@
+﻿param ([Parameter(Mandatory)]$runtime)
+
+$ErrorActionPreference = "Stop"
+
+Remove-Item -LiteralPath "$runtime.zip" -ErrorAction Ignore
+
+dotnet clean src/Ae.DiskUsage --configuration Release --runtime $runtime --framework net5.0
+
+dotnet publish src/Ae.DiskUsage --configuration Release --runtime $runtime --framework net5.0
+
+Compress-Archive -Path src/Ae.DiskUsage/bin/Release/net5.0/$runtime/publish/* -DestinationPath "$runtime.zip"
