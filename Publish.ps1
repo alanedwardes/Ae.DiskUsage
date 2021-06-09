860fa192a48c35cba9c3a2e9d11104eeb8a1c89b
Alan Edwardes <alan@alanedwardes.com>
2021-06-09 23:59:25 +0100

    More work on build script.

--- Publish.ps1
+++ Publish.ps1
@@ -6,6 +6,8 @@ Remove-Item -LiteralPath "$runtime.zip" -ErrorAction Ignore
 
 dotnet clean src/Ae.DiskUsage --configuration Release --runtime $runtime --framework net5.0
 
-dotnet publish src/Ae.DiskUsage --configuration Release --runtime $runtime --framework net5.0
+dotnet restore src/Ae.DiskUsage --runtime $runtime
+
+dotnet publish src/Ae.DiskUsage --configuration Release --runtime $runtime --framework net5.0 --no-restore
 
 Compress-Archive -Path src/Ae.DiskUsage/bin/Release/net5.0/$runtime/publish/* -DestinationPath "$runtime.zip"
