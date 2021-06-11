5b5384174138fd4387d4664a8fb6485b0f858849
Alan Edwardes <alan@alanedwardes.com>
2021-06-11 21:56:50 +0100

    Updates Publish.ps1 to package a single file app.

--- Publish.ps1
+++ Publish.ps1
@@ -4,10 +4,17 @@ $ErrorActionPreference = "Stop"
 
 Remove-Item -LiteralPath "$runtime.zip" -ErrorAction Ignore
 
-dotnet clean src/Ae.DiskUsage --configuration Release --runtime $runtime --framework net5.0
+dotnet clean src/Ae.DiskUsage
 
 dotnet restore src/Ae.DiskUsage --runtime $runtime
 
-dotnet publish src/Ae.DiskUsage --configuration Release --runtime $runtime --framework net5.0 --no-restore
+dotnet publish src/Ae.DiskUsage `
+    --configuration Release `
+    --runtime $runtime `
+    --framework net5.0 `
+    --no-restore `
+    --self-contained true `
+    -p:PublishSingleFile=true `
+    -p:PublishTrimmed=true
 
 Compress-Archive -Path src/Ae.DiskUsage/bin/Release/net5.0/$runtime/publish/* -DestinationPath "$runtime.zip"
